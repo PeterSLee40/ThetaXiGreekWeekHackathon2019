e8ee156989595bed1c0331ae6c9b21c9c5d71620
PeterLee <Peterslee40@yahoo.com>
2019-04-06 17:15:11 -0400

    added basic firebase

--- app/src/main/java/com/example/peterlee/thetaxigreekweek2019/MainActivity.java
+++ app/src/main/java/com/example/peterlee/thetaxigreekweek2019/MainActivity.java
@@ -3,16 +3,27 @@ package com.example.peterlee.thetaxigreekweek2019;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+//private FirebaseDatabase database;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, my dudes!");
     }
 
     //
-    protected void addData() {
 
+
+    protected void addMeal(Meal meal) {
+        //add meal to firebase.
     }
 }
