e14f596357e51f7a3607612657040af1710c847a
hcopp6 <hcopp6@gatech.edu>
2019-04-06 18:37:48 -0400

    Added Cuisine Colors

--- app/src/main/java/com/example/peterlee/thetaxigreekweek2019/Cuisine.java
+++ app/src/main/java/com/example/peterlee/thetaxigreekweek2019/Cuisine.java
@@ -1,20 +1,29 @@
 package com.example.peterlee.thetaxigreekweek2019;
 
+import android.graphics.Color;
+
 public enum Cuisine {
 
-    AMERICAN("American"),
-    ASIAN("Asian"),
-    EUROPEAN("European"),
-    LATIN("Latin"),
-    MIDDLE_EASTERN("Middle Eastern");
+    AMERICAN("American", "#F44336"),
+    ASIAN("Asian", "#673AB7"),
+    EUROPEAN("European", "#4CAF50"),
+    LATIN("Latin", "#FF9800"),
+    MIDDLE_EASTERN("Middle Eastern", "#FF5722");
 
     private final String cuisineName;
+    private final String cuisineColor;
 
-    Cuisine(String name) {
+    Cuisine(String name, String color) {
         cuisineName = name;
+        cuisineColor = color;
     }
 
     public String getName() {
         return cuisineName;
     }
+
+    public int getColor() {
+        return Color.parseColor(cuisineColor);
+    }
+
 }
