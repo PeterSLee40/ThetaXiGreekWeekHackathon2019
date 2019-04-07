1534497bd2e72f8f250c007432ce1939d35c9da6
PeterLee <Peterslee40@yahoo.com>
2019-04-06 22:02:26 -0400

    Update MainActivity.java

--- app/src/main/java/com/example/peterlee/thetaxigreekweek2019/MainActivity.java
+++ app/src/main/java/com/example/peterlee/thetaxigreekweek2019/MainActivity.java
@@ -24,10 +24,28 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //FirebaseDatabase database = FirebaseDatabase.getInstance();
-        //DatabaseReference myRef = database.getReference("message");
-
-        //myRef.setValue("Hello, my dudes!");
+        database = FirebaseDatabase.getInstance();
+        createFakeData();
+    }
+    public void createFakeData() {
+        Meal meal1 = new Meal("20190401", 40, MealEnum.MONDAY_LUNCH, Cuisine.AMERICAN);
+        Meal meal2 = new Meal("20190401", 60, MealEnum.MONDAY_DINNER, Cuisine.AMERICAN);
+        Meal meal3 = new Meal("20190402", 45, MealEnum.TUESDAY_LUNCH, Cuisine.LATIN);
+        Meal meal4 = new Meal("20190402", 55, MealEnum.TUESDAY_DINNER, Cuisine.MIDDLE_EASTERN);
+        Meal meal5 = new Meal("20190403", 50, MealEnum.WEDNESDAY_LUNCH, Cuisine.LATIN);
+        Meal meal6 = new Meal("20190403", 70, MealEnum.WEDNESDAY_DINNER, Cuisine.EUROPEAN);
+        Meal meal7 = new Meal("20190404", 40, MealEnum.THURSDAY_LUNCH, Cuisine.AMERICAN);
+        Meal meal8 = new Meal("20190404", 20, MealEnum.THURSDAY_DINNER, Cuisine.LATIN);
+        Meal meal9 = new Meal("20190405", 40, MealEnum.FRIDAY_LUNCH, Cuisine.EUROPEAN);
+        addMeal(meal1);
+        addMeal(meal2);
+        addMeal(meal3);
+        addMeal(meal4);
+        addMeal(meal5);
+        addMeal(meal6);
+        addMeal(meal7);
+        addMeal(meal8);
+        addMeal(meal9);
     }
 
     public void pickDate(MenuItem item) {
@@ -50,11 +68,15 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
 
     }
 
+    protected String bool2str(Boolean b) {
+        return b ? "1" : "0";
+    }
+
     //add meal to firebase.
     protected void addMeal(Meal meal) {
         //Toast.makeText(getApplicationContext(),meal.toString(),Toast.LENGTH_SHORT).show();
         myRef = database.getReference(meal.getDate()
-                + Boolean.toString(meal.getMealEnum().isDinner()));
+                + bool2str(meal.getMealEnum().isDinner()));
         myRef.setValue(meal);
     }
 
